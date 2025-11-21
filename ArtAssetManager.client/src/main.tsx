4daac637bb5ad5151a7122c8df2fddfd3012a369
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2025-11-21 12:39:15 +0100

    More changes to the template

--- ArtAssetManager.client/src/main.tsx
+++ ArtAssetManager.client/src/main.tsx
@@ -10,7 +10,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider>
-        <App />
+        <main className="dark">
+          <App />
+        </main>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,

856ebed879b438a2ef78b457cea7d20362c3838b
Kacper Adamczyk <kacpperr.adamczyk@gmail.com>
2025-11-27 16:38:31 +0100

    migrations: migrations with fixed db

---
+++ ArtAssetManager.Api/Migrations/20251127150521_FixScanFolderRelation.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtAssetManager.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixScanFolderRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assets_ScanFolders_ScanFolderId",
+                table: "Assets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assets_ScanFolders_ScanFolderId",
+                table: "Assets",
+                column: "ScanFolderId",
+                principalTable: "ScanFolders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assets_ScanFolders_ScanFolderId",
+                table: "Assets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assets_ScanFolders_ScanFolderId",
+                table: "Assets",
+                column: "ScanFolderId",
+                principalTable: "ScanFolders",
+                principalColumn: "Id");
+        }
+    }
+}
